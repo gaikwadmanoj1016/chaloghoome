35ded05228820b5ec43f06eec07ae07c84856234
gaikwadmanoj1016 <gaikwad.manoj1016@gmail.com>
2024-05-07 16:36:05 +0530

    Fix: first live without apis

---
+++ src/app/modules/landing-page/place-list/place-details/place-details.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ImagePreviewComponent } from '../../../../shared/image-preview/image-preview.component';
+import { ActivatedRoute } from '@angular/router';
+import { CommonService } from '../../../../services/common.service';
+import { ApiService } from '../../../../services/api.service';
+import { SharedModule } from '../../../../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+export interface PlaceDetails {
+  name: string;
+  description: string;
+  area: string;
+  yearEstablished: string;
+  uniqueFeature: string;
+  otherFeatures: string[];
+  highlights: Highlight[];
+  travelGuide: TravelGuide[];
+}
+
+export interface Highlight {
+  image: string;
+  title: string;
+  description: string;
+}
+
+export interface TravelGuide {
+  title: string;
+  para?: string;
+  bulletPoints?: any[];
+}
+
+@Component({
+  selector: 'app-place-details',
+  standalone: true,
+  imports: [SharedModule, HttpClientModule],
+  templateUrl: './place-details.component.html',
+  styleUrl: './place-details.component.scss'
+})
+export class PlaceDetailsComponent implements OnInit {
+  placeDetails: PlaceDetails | undefined;
+  placeId: number = 0;
+  constructor(private dialog: MatDialog, private route: ActivatedRoute, public commonService: CommonService, private apiRequest: ApiService) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((param: any) => {
+      if (param && param['placeId']) {
+        this.placeId = param['placeId'];
+        this.getPlaceDetails();
+      }
+    })
+  }
+
+  private getPlaceDetails() {
+    this.apiRequest.getPlaceDetails().subscribe((response: any) => {
+      console.log(response)
+      this.placeDetails = response.find((item: any) => item.id === Number(this.placeId));
+      console.log(this.placeDetails)
+    })
+  }
+  openPreview(url: string): void {
+    let imageUrl = url;
+    this.dialog.open(ImagePreviewComponent, {
+      data: { imageUrl },
+      panelClass: 'custom-dialog-container' // Optional custom styling
+    });
+  }
+
+
+}
