1c03930a8b113b9338c12f5768d54b172890ffa7
unknown <gaikwadmanoj1016>
2025-03-08 12:50:13 +0530

    fix: upload file url

--- environment.ts
+++ environment.ts
@@ -3,7 +3,8 @@ export class environment {
     // static assetUrl = 'http://localhost:8080/adminService/';
     // static assetUrl = 'http://localhost:8080/adminService/uploads/';
     static apiUrl = 'http://api.chaloghoome.com:8080/adminService/';
-    static assetUrl = 'http://api.chaloghoome.com:8080/adminService/';
+    static assetUrl = 'http://api.chaloghoome.com:8080/';
+    // static assetUrl = 'http://api.chaloghoome.com:8080/adminService/';
     // static webUrl: string = 'localhost:4200/';
     static webUrl: string = 'https://chaloghoome.com/';
 }
