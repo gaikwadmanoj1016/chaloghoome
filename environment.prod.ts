209b42c0579c6fea3d7e269729bd0d9dc27e1512
unknown <gaikwadmanoj1016>
2025-03-20 02:57:10 +0530

    fix: env

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,5 +1,5 @@
 export class environment {
-    static apiUrl = 'http://api.chaloghoome.com/adminService/';
+    static apiUrl = 'http://api.chaloghoome.com:8080/adminService/';
     static assetUrl = 'http://api.chaloghoome.com:8081/';
     static webUrl: string = 'https://chaloghoome.com/';
 }
