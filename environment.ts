2a19aac4d5a76ab3baf0182a079c4cdd29e21a4c
unknown <gaikwadmanoj1016>
2025-04-27 21:22:21 +0530

    env

--- environment.ts
+++ environment.ts
@@ -1,8 +1,8 @@
 export class environment {
-    static apiUrl = 'http://localhost:8081/adminService/';
+    // static apiUrl = 'http://localhost:8081/adminService/';
     // static assetUrl = 'http://localhost:8081/';
     // static webUrl: string = 'localhost:4200/';
-    // static apiUrl = 'https://api.chaloghoome.com/adminService/';
+    static apiUrl = 'https://api.chaloghoome.com/adminService/';
     static assetUrl = 'https://api.chaloghoome.com/';
     static webUrl: string = 'https://chaloghoome.com/';
 }
